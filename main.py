9773428e7fa0b70b2e17747c9cba9c0648d85c7d
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2025-02-21 11:37:35 +0530

    hks

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel,
                            QVBoxLayout, QHBoxLayout, QWidget, QMenu, QMenuBar,
                            QAction, QInputDialog)
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QFont, QPainter
-from PyQt5.QtMultimedia import QSound
+from PyQt5.QtGui import QFont
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtCore import QUrl
 
 class CircleButton(QPushButton):
     def __init__(self, text, parent=None):
         super().__init__(text, parent)
-        self.setFixedSize(60, 60)
+        self.setFixedSize(100, 100)
+        # Fixed stylesheet syntax by removing comments and fixing property values
         self.setStyleSheet("""
             QPushButton {
                 background-color: white;
-                border-radius: 30px;
+                border-radius: 50px;
                 color: black;
                 font-weight: bold;
+                font-size: 18pt;
             }
             QPushButton:hover {
                 background-color: #e0e0e0;
@@ -30,22 +33,24 @@ class TimerApp(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        # Set window properties
         self.setWindowTitle('Timer')
         self.setStyleSheet("background-color: black;")
-        self.setMinimumSize(400, 300)
+        self.setMinimumSize(800, 600)
         
-        # Create central widget and layout
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
+        layout.setSpacing(40)
+        layout.setContentsMargins(50, 50, 50, 50)
         
-        # Create menu bar
+        # Fixed menubar stylesheet
         menubar = QMenuBar()
         menubar.setStyleSheet("""
             QMenuBar {
                 background-color: black;
                 color: white;
+                font-size: 24pt;
+                padding: 10px;
             }
             QMenuBar::item:selected {
                 background-color: #404040;
@@ -53,12 +58,17 @@ class TimerApp(QMainWindow):
         """)
         self.setMenuBar(menubar)
         
-        # Create settings menu
+        # Fixed menu stylesheet
         settings_menu = QMenu('☰', self)
         settings_menu.setStyleSheet("""
             QMenu {
                 background-color: black;
                 color: white;
+                font-size: 20pt;
+                padding: 10px;
+            }
+            QMenu::item {
+                padding: 10px 20px;
             }
             QMenu::item:selected {
                 background-color: #404040;
@@ -66,23 +76,22 @@ class TimerApp(QMainWindow):
         """)
         menubar.addMenu(settings_menu)
         
-        # Add set timer action
         set_timer_action = QAction('Set Timer', self)
         set_timer_action.triggered.connect(self.setTimer)
         settings_menu.addAction(set_timer_action)
         
-        # Create timer display
+        # Timer display
         self.time_display = QLabel('00:00')
         self.time_display.setAlignment(Qt.AlignCenter)
         self.time_display.setStyleSheet("color: white;")
-        self.time_display.setFont(QFont('Arial', 72))
+        self.time_display.setFont(QFont('Arial', 200, QFont.Bold))
         layout.addWidget(self.time_display)
         
-        # Create button layout
+        # Buttons
         button_layout = QHBoxLayout()
         button_layout.setAlignment(Qt.AlignCenter)
+        button_layout.setSpacing(50)
         
-        # Create control buttons
         self.start_button = CircleButton('Start')
         self.pause_button = CircleButton('Pause')
         self.reset_button = CircleButton('Reset')
@@ -97,15 +106,16 @@ class TimerApp(QMainWindow):
         
         layout.addLayout(button_layout)
         
-        # Initialize timer
+        # Timer setup
         self.timer = QTimer()
         self.timer.timeout.connect(self.updateTimer)
         self.time_remaining = 0
         self.timer_running = False
         
-        # Set up sound
-        sound_file = Path('alarm.wav')  # Make sure to have this file in the same directory
-        self.alarm_sound = QSound(str(sound_file))
+        # Using QMediaPlayer instead of QSound
+        self.player = QMediaPlayer()
+        sound_file = Path('alarm.wav')
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(str(sound_file.absolute()))))
         
     def setTimer(self):
         minutes, ok = QInputDialog.getInt(self, 'Set Timer', 
@@ -141,7 +151,7 @@ class TimerApp(QMainWindow):
                 self.timer.stop()
                 self.timer_running = False
                 self.start_button.setEnabled(True)
-                self.alarm_sound.play()
+                self.player.play()
                 
     def updateDisplay(self):
         minutes = self.time_remaining // 60
